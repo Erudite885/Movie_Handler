6dd7259dcb6e31c60bddd2a597589f5a93cd4b14
Erudite885 <erudite885@gmail.com>
2023-01-13 12:16:10 +0100

    controlling the backdrop

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,11 +1,26 @@
 const addMovieModal = document.getElementById("add-modal");
 const startAddMovieBtn = document.querySelector("header button");
 const backDrop = document.getElementById("backdrop");
+const cancelAddMovieBtn = addMovieModal.querySelector(".btn--passive");
+
+const toggleBackDrop = () => {
+  backDrop.classList.toggle("visible");
+};
 
 const toggleMovieModal = () => {
+  toggleBackDrop();
   // addMovieModal.className = 'modal card'
-  backDrop.classList.toggle('visible');
   addMovieModal.classList.toggle("visible");
 };
 
+const backDropClickHandler = () => {
+  toggleMovieModal();
+};
+
+const cancelAddMovie = () => {
+  toggleMovieModal();
+};
+
 startAddMovieBtn.addEventListener("click", toggleMovieModal);
+backDrop.addEventListener("click", backDropClickHandler);
+cancelAddMovieBtn.addEventListener("click", cancelAddMovie);

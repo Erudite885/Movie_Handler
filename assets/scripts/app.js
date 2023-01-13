55729670232c4699ec608e7a2d9af31636cfb408
Erudite885 <erudite885@gmail.com>
2023-01-13 12:30:04 +0100

    fetchinf and validating user input

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,25 +2,45 @@ const addMovieModal = document.getElementById("add-modal");
 const startAddMovieBtn = document.querySelector("header button");
 const backDrop = document.getElementById("backdrop");
 const cancelAddMovieBtn = addMovieModal.querySelector(".btn--passive");
+const confirmAddMovieBtn = cancelAddMovieBtn.nextElementSibling;
+const userInputs = addMovieModal.querySelectorAll("input");
+// const userInputs = addMovieModal.getElementsByTagName('input');
 
 const toggleBackDrop = () => {
   backDrop.classList.toggle("visible");
 };
 
-const toggleMovieModal = () => {
+const toggleMovieModalHandler = () => {
   toggleBackDrop();
   // addMovieModal.className = 'modal card'
   addMovieModal.classList.toggle("visible");
 };
 
 const backDropClickHandler = () => {
+  toggleMovieModalHandler();
+};
+
+const cancelAddMovieHandler = () => {
   toggleMovieModal();
 };
 
-const cancelAddMovie = () => {
-  toggleMovieModal();
+const addMovieHandler = () => {
+  const titleVal = userInputs[0].value;
+  const imgUrlVal = userInputs[1].value;
+  const ratingVal = userInputs[2].value;
+
+  if (
+    titleVal.trim() === "" ||
+    imgUrlVal.trim() === "" ||
+    ratingVal.trim() === "" ||
+    +ratingVal < 1 ||
+    +ratingVal > 5
+  ) {
+    alert("Please Enter Valid Values (RATING between 1 and 5)");
+  }
 };
 
-startAddMovieBtn.addEventListener("click", toggleMovieModal);
+startAddMovieBtn.addEventListener("click", toggleMovieModalHandler);
 backDrop.addEventListener("click", backDropClickHandler);
-cancelAddMovieBtn.addEventListener("click", cancelAddMovie);
+cancelAddMovieBtn.addEventListener("click", cancelAddMovieHandler);
+confirmAddMovieBtn.addEventListener("click", addMovieHandler);
